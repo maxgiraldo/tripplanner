8ee71e2ccb758145836d2bfe45f390e4809f22bc
Max Giraldo <dr.datacode@gmail.com>
2014-02-16 16:20:56 -0500

    add days to trip, delete items from list

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,7 +2,7 @@
 
 $( document ).ready(function() {
 
-// Maps
+// Maps Initialization
 	var myLatlng, mapOptions, map;
 
 	function initialize() {
@@ -14,7 +14,6 @@ $( document ).ready(function() {
         };
         map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
-
       }
 
       google.maps.event.addDomListener(window, 'load', initialize);
@@ -25,19 +24,28 @@ $( document ).ready(function() {
 	    map: map,
 	    title: data.place[0].name
 		});
-		// return marker;
-	}
 
+	}
 // End maps
 
 
+// Add items to day plan, pin locations on map
 	$('#hotel-submit').one('click', function() {
 		var selectedHotel = $("#hotels-select option:selected");
 		var hotelId = selectedHotel.val();
 		var hotelData = _.findWhere(all_hotels, {_id: hotelId});
 		addMarker(hotelData);
+
 		var li = "<li>" + selectedHotel.text() + "</li>";
 		$('#hotel-list').append(li);
+
+		var deleteItem = "<a href='#' class='deleteItem'>Delete</a>";
+		$('#hotel-list > li').append(deleteItem);
+
+		$('a.deleteItem').click(function(event) {
+			$( event.target ).closest( 'li' ).remove();
+		});
+
 	});
 
 	$('#thingtodo-submit').click(function() {
@@ -45,23 +53,43 @@ $( document ).ready(function() {
 		var thingId = selectedThingToDo.val();
 		var thingData = _.findWhere(all_things_to_do, {_id: thingId});
 		addMarker(thingData);
-		var li = "<li>" + selectedThingToDo.text() + "</li>";
+
+		var deleteItem = "<a class='deleteItem'>Delete</a>";
+		var li = "<li>" + selectedThingToDo.text() + deleteItem + "</li>";
 		$('#thingstodo-list').append(li);
+
+		$('a.deleteItem').click(function(event) {
+			$( event.target ).closest( 'li' ).remove();
+		});
 	});
 
-	var addLimit = 0;
+	var restaurantAddLimit = 0;
 	$('#restaurant-submit').click(function() {
-		if (addLimit >= 3) return;
+		if (restaurantAddLimit >= 3) return;
 
 		var selectedRestaurant = $("#restaurants-select option:selected");
 		var restaurantId = selectedRestaurant.val();
 		var restaurantData = _.findWhere(all_restaurants, {_id: restaurantId});
 		addMarker(restaurantData);
 
-		var li = "<li>" + selectedRestaurant.text() + "</li>";
+		var deleteItem = "<a class='deleteItem'>Delete</a>";
+		var li = "<li>" + selectedRestaurant.text() + deleteItem + "</li>";
 		$('#restaurant-list').append(li);
 
+		$('a.deleteItem').click(function(event) {
+			$( event.target ).closest( 'li' ).remove();
+		});
+
 		addLimit++;
+	});
+
+	$('#add-day').click(function() {
+		var previousElement = $('#add-day').prev();
+		var newValue = previousElement.val();
+		newValue++;
+
+		var newButton = "<button type='button' class='btn btn-default' value=" + newValue + ">" + "Day" + newValue + "</button>";
+		$(previousElement).after(newButton);
 	});
 
 });
